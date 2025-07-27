c6d9b0d4de08f864ab8a053157f9d47009aada0d
kelvin <kelvinperrie@gmail.com>
2025-07-27 14:57:00 +1200

    getting sound files okey dokey

--- scripts/soundconfig.js
+++ scripts/soundconfig.js
@@ -1,11 +1,11 @@
 var soundsConfiguration = [
     {
-        "label": "North Island Brown Kiwi",
+        "label": "North Island Brown Kiwi male",
         "sounds" : [
             {
-                "file" : "sounds/NI Brown Kiwi - 69494291 - linds-nz-altered2.mp3",
-                "label" : "North Island Brown Kiwi",
-                "initialVolume" : 0.5,
+                "file" : "sounds/North Island Brown Kiwi male - ob69494291 - linds-nz - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi male",
+                "initialVolume" : 1,
                 "attribution" : {
                     "name" : "Lindsay Alexander",
                     "link" : "https://inaturalist.nz/observations/69494291",
@@ -13,53 +13,162 @@ var soundsConfiguration = [
                 }
             },
             {
-                "file" : "sounds/Brown Kiwi - Lidsay Alexander - httpsmacaulaylibrary.org-asset-175323911.mp3",
-                "label" : "NI Brown Kiwi (male + female)",
-                "initialVolume" : 0.5,
+                "file" : "sounds/North Island Brown Kiwi male - ob146752581 - benjanss - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi male",
+                "initialVolume" : 1,
                 "attribution" : {
-                    "name" : "Lindsay Alexander",
-                    "link" : "https://macaulaylibrary.org/asset/175323911",
+                    "name" : "Benjamin Wright",
+                    "link" : "https://inaturalist.nz/observations/146752581",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/North Island Brown Kiwi male - ob195030148 - oscar_dove - cc by - altered.mp3",
+                "label" : "North Island Brown Kiwi male",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "Oscar Dove",
+                    "link" : "https://inaturalist.nz/observations/195030148",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/North Island Brown Kiwi male - ob244447007 - glebglub - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi male",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "A Xiong",
+                    "link" : "https://inaturalist.nz/observations/244447007",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/North Island Brown Kiwi male - ob265930867 - swisskiwi71 - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi male",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "SwissKiwi",
+                    "link" : "https://inaturalist.nz/observations/265930867",
                     "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
                 }
             }
         ]
     },
     {
-        "label": "Little Spotted Kiwi",
+        "label": "North Island Brown Kiwi female",
         "sounds" : [
             {
-                "file" : "sounds/Little Spotted Kiwi - tony_wills - by - 560897.mp3",
-                "label" : "Little Spotted Kiwi",
-                "initialVolume" : 0.6,
+                "file" : "sounds/North Island Brown Kiwi female - ob33343460 - mad_wild - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi female",
+                "initialVolume" : 1,
                 "attribution" : {
-                    "name" : "Tony Wills",
-                    "link" : "https://inaturalist.nz/observations/560897",
-                    "licenseUrl" : "https://creativecommons.org/licenses/by/4.0/"
+                    "name" : "Madara Vilde",
+                    "link" : "https://inaturalist.nz/observations/33343460",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/North Island Brown Kiwi female - ob69494291 - linds-nz - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi female",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "Lindsay Alexander",
+                    "link" : "https://macaulaylibrary.org/asset/69494291",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/North Island Brown Kiwi female - ob146752581 - benjanss - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi female",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "Benjamin Wright",
+                    "link" : "https://macaulaylibrary.org/asset/146752581",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/North Island Brown Kiwi female - ob196098398 - rolandwirth - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi female",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "rolandwirth",
+                    "link" : "https://macaulaylibrary.org/asset/196098398",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/North Island Brown Kiwi female - ob265930867 - swisskiwi71 - cc by-nc - altered.mp3",
+                "label" : "North Island Brown Kiwi female",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "SwissKiwi",
+                    "link" : "https://macaulaylibrary.org/asset/265930867",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
                 }
             }
-
         ]
     },
     {
         "label": "Ruru",
         "sounds" : [
             {
-                "file" : "sounds/Ruru - 102334719 - linds-nz.mp3",
-                "label" : "Ruru squawk",
-                "initialVolume" : 0.5,
+                "file" : "sounds/Ruru - ob261956468 - cmyplay - cc-by-nc - altered.mp3",
+                "label" : "Ruru",
+                "initialVolume" : 1,
                 "attribution" : {
-                    "name" : "Lindsay Alexander",
-                    "link" : "https://inaturalist.nz/observations/102334719",
+                    "name" : "cmyplay",
+                    "link" : "https://inaturalist.nz/observations/261956468",
                     "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
                 }
             },
             {
-                "file" : "sounds/Ruru - 100382629 - andrewmaungakotukutuku-altered.mp3",
-                "label" : "Ruru / Morepork",
-                "initialVolume" : 0.5,
+                "file" : "sounds/Ruru cree - ob254077870 - jupal04 - cc-by - altered.mp3",
+                "label" : "Ruru cree",
+                "initialVolume" : 1,
                 "attribution" : {
-                    "name" : "andrewmaungakotukutuku",
-                    "link" : "https://inaturalist.nz/observations/100382629",
+                    "name" : "Julia Palmer",
+                    "link" : "https://inaturalist.nz/observations/254077870",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/Ruru cree - ob273742010 - ryvesie - cc-by-nc - altered.mp3",
+                "label" : "Ruru cree",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "Joanne Ryves",
+                    "link" : "https://inaturalist.nz/observations/273742010",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/Ruru cree - ob288146854 - robynn - cc-by-nc -altered.mp3",
+                "label" : "Ruru cree",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "Robyn",
+                    "link" : "https://inaturalist.nz/observations/288146854",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/Ruru scree - ob260130123 - esteban_uc - cc-by-nc - altered.mp3",
+                "label" : "Ruru cree",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "esteban_uc",
+                    "link" : "https://inaturalist.nz/observations/260130123",
+                    "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
+                }
+            },
+            {
+                "file" : "sounds/Ruru squeal - ob272937719 - dbiaggio - cc-by-nc - altered.mp3",
+                "label" : "Ruru cree",
+                "initialVolume" : 1,
+                "attribution" : {
+                    "name" : "Daniela Biaggio",
+                    "link" : "https://inaturalist.nz/observations/272937719",
                     "licenseUrl" : "https://creativecommons.org/licenses/by-nc/4.0/"
                 }
             }
